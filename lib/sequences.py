c984c5b8a991039465515fe3d2c80c92edbf3ab9
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-05-17 14:21:31 +0300

    Done with fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0,1]
+    while len(sequence)< length:
+        next_num= sequence[-1]+sequence[-2]
+        sequence.append(next_num)
+    print(sequence)
+print_fibonacci(9)
